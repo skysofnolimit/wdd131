7a4c8d734df55338af6f1354c7fc2e2e34986239
skysofnolimit <168154937+skysofnolimit@users.noreply.github.com>
2024-10-03 19:14:55 -0400

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,5 @@
 const year = document.querySelector("#year");
-
+const today = new Date();
+).format(today)}</span>';
 year.innerHTML = `getFullYear(): <span class="highlight">${today.getFullYear()}</span>`;
 
