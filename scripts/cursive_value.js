e62a3c9b58aad19e701c567b0a9b1fbbee4c58f3
skysofnolimit <168154937+skysofnolimit@users.noreply.github.com>
2024-10-30 14:48:12 -0400

    Update cursive_value.js

--- scripts/cursive_value.js
+++ scripts/cursive_value.js
@@ -1,1 +1,16 @@
-const
+constconst hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const mainContainer = document.querySelector(".main_container");
+
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  mainContainer.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  mainContainer.classList.remove("active");
+}))
